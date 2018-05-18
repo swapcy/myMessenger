1bf7610cc3cab647ddf72d19cdd5a8197f039c9f
swapnilvc <emailtoswap@gmail.com>
2018-05-18 14:40:53 +0530

    twitter bot 5

--- app.py
+++ app.py
@@ -8,16 +8,19 @@ in any messages that the bot receives and echos it back.
 
 from flask import Flask, request
 from pymessenger.bot import Bot
+from twurl import extractTweets
+import random
 
 app = Flask(__name__)
 
 ACCESS_TOKEN = "EAAEFka4KZCEMBAPsugFglElkiuw2scGO25Y4FXCUOcCLRuzmvX5sTjLhOOtdTHqpLS0s4N1OfltvMGGZCRsRMdoSUNAa13HGaisZCXwNGzNQ2ID8AlXGPlSFCsHsupMyWHqsf9YrZBrSvlRb4FPyrMh0kNXhpBjaQWeaS8B20gZDZD"
 VERIFY_TOKEN = "hello"
 
+#emoji = [':)',':D','(Y)',':(']
+
 bot = Bot(ACCESS_TOKEN)
 
 @app.route("/", methods=['GET', 'POST'])
-
 def hello():
     if request.method == 'GET':
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
@@ -32,19 +35,37 @@ def hello():
             print('message received!')
             messaging = event['messaging']
             for x in messaging:
-                if x.get('message'):
+                if x.get('message'):    
                     recipient_id = x['sender']['id']
                     if x['message'].get('text'):
                         message = x['message']['text']
-                        bot.send_text_message(recipient_id, message)
+                        print("Calling getResponse: "+message)
+                        resp = getResponse(message)
+                        for r in resp:
+                            bot.send_text_message(recipient_id, r)                          
                     if x['message'].get('attachments'):
-                        for att in x['message'].get('attachments'):
-                            bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
+                        bot.send_text_message(recipient_id, 'I cannot handle attachments')                          
                 else:
                     pass
             print('Outside for')
         return "Success"
 
+def getResponse(message):
+    try:
+        #tweets = ['hi','my name is ',message]
+        tweets = extractTweets(message)
+        return tweets
+        
+    except Exception as e:
+        return 'Error Occured'+str(e)
+    else:
+        pass
+    finally:
+        pass
+
+
 
 if __name__ == "__main__":
     app.run(port=80, debug=True)
+
+
---
+++ twurl.py
@@ -0,0 +1,59 @@
+
+import oauth2 as oauth
+import json
+import hidden
+
+
+secret = hidden.oauth()
+
+CONSUMER_KEY = secret['consumer_key']
+CONSUMER_SECRET = secret['consumer_secret']
+ACCESS_KEY = secret['token_key']
+ACCESS_SECRET = secret['token_secret']
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
+
+search_tweets_url = 'https://api.twitter.com/1.1/search/tweets.json'
+
+#print(response)
+
+#print(data)
+
+
+def extractTweets(param='IPL'):
+    print("Grabbing tweets for "+param)
+    parameter = param
+    search_tweets = search_tweets_url+'?q='+parameter+'&result_type=recent&count=5'
+    try:
+        output = []
+        response, data = client.request(search_tweets)
+        print('Here are the recent 5 tweets on :{0} \n '.format(' '+param))
+        output.append('Here are the recent 5 tweets on :{0} \n '.format(' '+param))
+        tweets = json.loads(data)
+        for tweet in tweets['statuses']:
+            #print('\n'+tweet['user']['screen_name']+' : '+tweet['text']+'\n')
+            output.append(tweet['user']['screen_name']+' : '+tweet['text'])            
+            #for count in range(20):
+             #   print('-',end='')
+        print(output)
+        return output
+    except Exception as e:
+        print('Something went wrong :( '+str(e))
+        return ['Something went wrong! :(']
+    else:
+        print(':D')
+        return ['Else :D']
+    finally:
+        pass
+
+
+#extractTweets(param='ipl')
+
+'''with open('data.txt', 'w') as outfile:
+     json.dump(tweets, outfile, sort_keys = True, indent = 4)'''
+
+
+
+
